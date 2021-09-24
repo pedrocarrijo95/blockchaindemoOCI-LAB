dd8d2126d33c785863d7a34cfdafc38a8c581cd1
pedrocarrijo <pedro.carrijo@oracle.com>
2021-09-24 11:11:49 -0300

    finalizado

--- public/js/globalvars.js
+++ public/js/globalvars.js
@@ -1,8 +1,8 @@
 /** MUDAR AS INFOS AQUI PARA A INFO DO SEU AMBIENTE */
 var variables = {
-    url: "http://129.213.202.34/restproxy",
-    channel: "testesmart",
-    chaincodename: "oabcs-testesmart",
-    chaincodeVer: "v2"
+    url: "SUA URL DO RESTPROXY BLOCKCHAIN",
+    channel: "NOME_CHANNEL",
+    chaincodename: "NOME_SMARTCONTRACT(CHAINCODE)",
+    chaincodeVer: "VERSÃO CHAINCODE"
 };
 export { variables };
